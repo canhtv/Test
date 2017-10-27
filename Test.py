876ef960aba91fed60b7e1d3759fc417e0cb1223
Tran Van Canh <canhtv@gmail.com>
2017-10-27 22:19:41 +0200

    add linear regresion model

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 
+import sklearn.linear_models.LinearRegression
 import random
 import matplotlib.pyplot as plt
 #%matplotlib inline
@@ -19,6 +20,15 @@ for i in range(2,16):
     colname = 'x_%d'%i
     data[colname] = x**i
 #print data.head()
-plt.plot(data['x'],data['y'],'.')
-plt.show()
+#plt.plot(data['x'],data['y'],'.')
+#plt.show()
 
+features = ['x', 'x_%d'%i for i in range(2,16)]
+X = data[features].values
+
+model = LinearRegression()
+model.fit(X,y)
+
+out = model.predict(X)
+
+print sklearn.metrics.mean_squared_error(y,out)
